2922eed1a010f24f9e752f499a78cf8fdd56c6be
Thayroni-Lima <thayronivflima@gmail.com>
2025-10-29 15:05:25 -0300

    [INFRA] conteinerização de todo o projeto atualiza serviços de usuário e autenticação para usar variável de ambiente VITE_API_URL; adiciona arquivos de configuração do Docker e Nginx

--- frontend-campanha/src/services/authService.js
+++ frontend-campanha/src/services/authService.js
@@ -1,7 +1,7 @@
 // src/services/AuthService.js
 import axios from "axios";
 
-const API_URL = "http://localhost:8080/auth"; 
+const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:8080';
 
 export async function login(credentials) {
   try {
